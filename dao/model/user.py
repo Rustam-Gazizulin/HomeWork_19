d94ebee1668431e94ddd9c98be06dddb0f27d55f
Rustam <103527813+Rustam-Gazizulin@users.noreply.github.com>
2022-07-08 15:04:32 +0500

    init

---
+++ dao/model/user.py
@@ -0,0 +1,14 @@
+from marshmallow import Schema, fields
+
+from setup_db import db
+
+
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+
+
+class UserSchema(Schema):
+    id = fields.Int()
+    name = fields.Str()
