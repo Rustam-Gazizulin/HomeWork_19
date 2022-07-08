de3cd1779a7c21f45065db32e2d6af35f288de69
Rustam <103527813+Rustam-Gazizulin@users.noreply.github.com>
2022-07-08 15:08:18 +0500

    add model user

--- dao/model/user.py
+++ dao/model/user.py
@@ -6,9 +6,14 @@ from setup_db import db
 class User(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255))
+    username = db.Column(db.String(255))
+    password = db.Column(db.String(255))
+    role = db.Column(db.String(255))
 
 
 class UserSchema(Schema):
     id = fields.Int()
-    name = fields.Str()
+    username = fields.Str()
+    password = fields.Str()
+    role = fields.Str()
+
